64189f2538e5274b1a9cb53d969e021f3e317152
Ray Ortiz <oray@bgsu.edu>
2023-01-22 11:56:39 -0500

    unit test the withdraw function

--- test/faucetTests.js
+++ test/faucetTests.js
@@ -1,23 +1,34 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { expect } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Faucet', function () {
+describe("Faucet", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployContractAndSetVariables() {
-    const Faucet = await ethers.getContractFactory('Faucet');
+    const Faucet = await ethers.getContractFactory("Faucet");
     const faucet = await Faucet.deploy();
 
     const [owner] = await ethers.getSigners();
 
-    console.log('Signer 1 address: ', owner.address);
-    return { faucet, owner };
+    let withdrawAmount = ethers.utils.parseUnits("1", "ether");
+
+    console.log("Signer 1 address: ", owner.address);
+    return { faucet, owner, withdrawAmount };
   }
 
-  it('should deploy and set the owner correctly', async function () {
-    const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
+  it("should deploy and set the owner correctly", async function () {
+    const { faucet, owner, Signer2 } = await loadFixture(
+      deployContractAndSetVariables
+    );
 
     expect(await faucet.owner()).to.equal(owner.address);
+  });
+  it("should not allow withdrawls above .1 ETH at a time", async function () {
+    const { faucet, withdrawAmount } = await loadFixture(
+      deployContractAndSetVariables
+    );
+    await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
   });
 });
