675f00bbff97b7f40242e8a4e9b23a5d873d84cb
Ray Ortiz <oray@bgsu.edu>
2023-01-23 07:48:25 -0500

    final challenge complete

--- test/faucetTests.js
+++ test/faucetTests.js
@@ -1,24 +1,37 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
-const { assertHardhatInvariant } = require("hardhat/internal/core/errors");
+const BigNumber = require("bignumber.js");
 
 describe("Faucet", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployContractAndSetVariables() {
+    let deposit = ethers.utils.parseUnits("1", "ether");
     const Faucet = await ethers.getContractFactory("Faucet");
-    const faucet = await Faucet.deploy();
+    const faucet = await Faucet.deploy({ value: deposit }); //sending 1 ETH, which is used in the last it test.
 
     const [owner, signer2] = await ethers.getSigners();
 
     let withdrawAmount = ethers.utils.parseUnits("1", "ether");
 
+    let contractBalanceBefore = await ethers.provider.getBalance(
+      faucet.address
+    );
+    let ownerBalanceBefore = await ethers.provider.getBalance(owner.address);
+    console.log(ownerBalanceBefore);
     // console.log("Signer 1 address: ", owner.address);
     // console.log("Signer 2 address: ", signer2.address);
     // console.log(faucet.address);
-    return { faucet, owner, withdrawAmount, signer2 };
+    return {
+      faucet,
+      owner,
+      withdrawAmount,
+      signer2,
+      contractBalanceBefore,
+      ownerBalanceBefore,
+    };
   }
 
   it("should deploy and set the owner correctly", async function () {
@@ -44,5 +57,14 @@ describe("Faucet", function () {
     const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
     await faucet.destroyFaucet();
     expect(await ethers.provider.getCode(faucet.address)).to.equal("0x");
+  });
+  it("should check that the withdrawAll() function successfully return all of the ether held in the smart contract", async function () {
+    const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
+
+    await faucet.withdrawAll();
+    console.log(await ethers.provider.getBalance(owner.address));
+    expect(
+      await (await ethers.provider.getBalance(owner.address)).toString()
+    ).to.equal("9999999226039357158946");
   });
 });
